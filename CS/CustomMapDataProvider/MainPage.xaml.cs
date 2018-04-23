fe025c98f03ad938226e47a171000d20bd9c3d1e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomMapDataProvider/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Map;
+using System.Globalization;
+
+namespace CustomMapDataProvider {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            imageTilesLayer.DataProvider = new CustomMapDataProvider();
+        }
+
+        public class CustomMapDataProvider : MapDataProviderBase {
+            readonly SphericalMercatorProjection projection = new SphericalMercatorProjection();
+
+            public override ProjectionBase Projection { get { return projection; } }
+
+            public CustomMapDataProvider() {
+                SetTileSource(new CustomTileSource());
+            }
+            protected override MapDependencyObject CreateObject() {
+                return new CustomMapDataProvider();
+            }
+            public override Size GetMapSizeInPixels(double zoomLevel) {
+                return new Size(Math.Pow(2.0, zoomLevel) * OpenStreetMapTileSource.tileSize,
+                    Math.Pow(2.0, zoomLevel) * OpenStreetMapTileSource.tileSize);
+            }
+        }
+
+        public class CustomTileSource : MapTileSourceBase {
+            const string roadUrlTemplate =
+                @"http://{subdomain}.tile.openstreetmap.org/{tileLevel}/{tileX}/{tileY}.png";
+            public const int maxZoomLevel = 20;
+            public const int tileSize = 256;
+
+            static int imageWidth = (int)Math.Pow(2.0, maxZoomLevel) * tileSize;
+            static int imageHeight = (int)Math.Pow(2.0, maxZoomLevel) * tileSize;
+            static string[] subdomains = new string[] { "a", "b", "c" };
+
+            public CustomTileSource()
+                : base(imageWidth, imageHeight, tileSize, tileSize) {
+
+            }
+
+            public override Uri GetTileByZoomLevel(int zoomLevel, int tilePositionX, int tilePositionY) {
+                string url = roadUrlTemplate;
+                url = url.Replace("{tileX}", tilePositionX.ToString(CultureInfo.InvariantCulture));
+                url = url.Replace("{tileY}", tilePositionY.ToString(CultureInfo.InvariantCulture));
+                url = url.Replace("{tileLevel}", zoomLevel.ToString(CultureInfo.InvariantCulture));
+                url = url.Replace("{subdomain}", subdomains[GetSubdomainIndex(subdomains.Length)]);
+                return new Uri(url);
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to implement a custom map data provider
+
+
+<p>This example illustrates how to implement a custom map data provider class inherited from the <a href="http://documentation.devexpress.com/#Silverlight/clsDevExpressXpfMapMapDataProviderBasetopic"><u>MapDataProviderBase</u></a> type. In the example, a custom <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapMapTileSourceBase_ctortopic"><u>MapTileSourceBase</u></a> class is implemented to supply tile URLs from the <a href="http://www.openstreetmap.org/"><u>openstreetmap.org</u></a> source.</p><p><br />
+</p>
+
+<br/>
+
+
